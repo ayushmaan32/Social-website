2d20381c748d60629a47de2ab8d2bb35cd66c818
ayushmaan <ayushmaanroy.12@gmail.com>
2022-12-04 22:12:43 +0530

    setup static file access for layout and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,8 @@
 module.exports.home = function(req,res){
-    return res.end('<h1> Express is up for cordial!');
+    // return res.end('<h1> Express is up for cordial!');
+
+    return res.render('home',{
+        title:"home"
+    })
 }
 // module.exports.actionname = function(req,res){}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile = function(req,res){
-    return res.end('<h1>profile is visible</h1>');
+    // return res.end('<h1>profile is visible</h1>');
+
+    return res.render('users', {
+        title : 'Users '
+    })
 }
