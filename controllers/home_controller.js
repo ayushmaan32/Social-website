e9b15fe47111e1900ec05a1f7fe01aa06e582b3e
ayushmaan <ayushmaanroy.12@gmail.com>
2022-12-11 14:02:42 +0530

    display post on users page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,27 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
     // return res.end('<h1> Express is up for cordial!');
-    console.log(req.cookies);
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+// Post.find({},function(err,posts){
+//     return res.render('home',{
+//         title:"Codial | home",
+//         posts: posts
+//     })
+// })
+//populate the user of each post
+Post.find({})
+.populate('user')
+.exec(function(err,posts){
+    return res.render('home',{
+        title:"Codial | home",
+        posts: posts
+    });
+})
+
     
 
-    return res.render('home',{
-        title:"home"
-    })
+   
 }
 // module.exports.actionname = function(req,res){}
