91f20b2bd2154e6ec1e071266da263a84633ab13
ayushmaan <ayushmaanroy.12@gmail.com>
2022-12-12 22:44:57 +0530

    deleting comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,22 +1,40 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req,res){
-    Post.findById(req.body.post, function(err,post){
+module.exports.create = function (req, res) {
+    Post.findById(req.body.post, function (err, post) {
         //handle the err
-        if(post){
+        if (post) {
             Comment.create({
-                content:req.body.content,
-                post:req.body.post,
+                content: req.body.content,
+                post: req.body.post,
                 user: req.user._id
             },
-            function(err,comment){
-                post.comments.push(comment);
-                post.save();
+                function (err, comment) {
+                    post.comments.push(comment);
+                    post.save();
 
-               return res.redirect('/');
-            }
+                    return res.redirect('/');
+                }
             );
         }
+    })
+}
+
+module.exports.destroy = function (req, res) {
+    Comment.findById(req.params.id, function (err, comment) {
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id}}, function(err, post) {
+                return res.redirect('back');
+            }
+             
+                
+              )
+           }
+        else {
+    return res.redirect('back')
+}
     })
 }
